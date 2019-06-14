bc96c06fabb1bf585ec07dd85a60b22f8587b9e9
zthxxx <zthxxx.me@gmail.com>
2019-06-14 20:22:00 +0800

    fix: update checkin url

--- checker.sh
+++ checker.sh
@@ -2,12 +2,26 @@
 
 __COMMENTS__='
 # OS: ubuntu 14.04 trusty
-# daliy check for the magic of kura chan in magic.moe.lu which is own by Sanzenin Kura (��ǧԺ�ΰ�)
+# daliy check for the magic of kura chan in magic.moe.lu which is own by Sanzenin Kura (三千院の暗)
 # its a simple demo with one line command,
-# need "KuraChan_Magic_Cookies" variable in env.
+# need "auth_email" "auth_passwd" variable in env.
 '
-echo "In this check you got transfer: $( \
-    curl -s -b "${KuraChan_Magic_Cookies}" "https://xn--nwwv50h.xn--yety8v.com/user/_checkin.php" | \
-    grep -o -P '(?<=\\u83b7\\u5f97\\u4e86)(.*)(?=\\u6d41\\u91cf)' \
-)"
 
+cookies=`mktemp -t kura-XXX.cookies`
+kura_domain='https://xn--nwwv50h.xn--yety8v.com'
+
+login=$(
+    curl -s -c "${cookies}" -b "${cookies}" "${kura_domain}/auth/login" \
+        --data-urlencode "email=${auth_email}" \
+        --data-urlencode "passwd=${auth_passwd}"
+)
+
+echo "login:"
+echo -e "$login"
+
+# checkin response schema
+# {"msg":"\u83b7\u5f97\u4e86 500 MB\u6d41\u91cf.","ret":1}
+checkin="`curl -s -c "${cookies}" -b "${cookies}" -X POST "${kura_domain}/user/checkin"`"
+
+echo "checkin"
+echo -e "$checkin"
